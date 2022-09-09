2624dd3e90e6a575c847b2b31bbbaa3f1ff16b41
rramosx11 <rodolforamosd11@gmail.com>
2022-09-09 03:43:49 -0400

    added jawsql

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 
 // create our User model
 class User extends Model {
-  // set up method to run on instance data (per user) to check password
+  // set up method to run on instance data (per user) to check passwordddd
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
