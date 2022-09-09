d4f28f4c1a1bc97cb49f9de2aeedad26d1b06a31
rramosx11 <rodolforamosd11@gmail.com>
2022-09-09 04:00:09 -0400

    added a comment in connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-// create connection to our database, pass in your MySQL information for username and password
+// create connection to our database, pass in your MySQL information for username and passworddsdksjflsdjflksdj
 
 const sequelize = process.env.JAWSDB_URL
   ? (Sequelize = process.env.JAWSDB_URL)
