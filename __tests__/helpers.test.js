ef61254c005fe451228d8efd395f7f3351825e73
rramosx11 <rodolforamosd11@gmail.com>
2022-09-15 21:47:38 -0400

    added the post content and timer

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,11 +1,11 @@
 const { format_date } = require("../utils/helpers");
-const { format_plural } = require("../utils/helpers");
 
 test("format_date() returns a date string", () => {
   const date = new Date("2020-03-20 16:12:03");
 
   expect(format_date(date)).toBe("3/20/2020");
 });
+const { format_plural } = require("../utils/helpers");
 
 test("format_plural() returns a pluralized word", () => {
   const word1 = format_plural("tiger", 1);
